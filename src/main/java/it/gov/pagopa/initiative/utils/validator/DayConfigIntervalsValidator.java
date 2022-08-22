0c6c1ae0f29ee18d11e0c4e24fb248f26ee02582
Francesco Pinsone <francesco.pinsone.st@everis.nttdata.com>
2022-08-22 17:57:48 +0200

    reward & trx rule test on going for coverage increment

--- src/main/java/it/gov/pagopa/initiative/utils/validator/DayConfigIntervalsValidator.java
+++ src/main/java/it/gov/pagopa/initiative/utils/validator/DayConfigIntervalsValidator.java
@@ -26,7 +26,7 @@ public class DayConfigIntervalsValidator implements ConstraintValidator<DayConfi
             return false;
         }
         for (int j = 0; j < listTmp.size()-1; j++){
-            for (int i = j+1; i < listTmp.size()-1; i++){
+            for (int i = j+1; i <= listTmp.size()-1; i++){
                 if (listTmp.get(j).getStartTime().equals(listTmp.get(i).getStartTime()) && listTmp.get(j).getEndTime().equals(listTmp.get(i).getEndTime())){
                     return false;
                 }
