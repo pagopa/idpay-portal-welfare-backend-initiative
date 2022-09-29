f5f03dbc786c51f65ac20b570480c11a8f836a79
Stefano Terragni <stefano.terragni@everis.com>
2022-09-29 16:43:57 +0200

    [API-Publish] Coverage: new case convert InitiativeDTO into Initiative

--- src/test/java/it/gov/pagopa/initiative/mapper/InitiativeDTOsToModelMapperTest.java
+++ src/test/java/it/gov/pagopa/initiative/mapper/InitiativeDTOsToModelMapperTest.java
@@ -97,6 +97,10 @@ class InitiativeDTOsToModelMapperTest {
 
     private Initiative initiativeTrxRuleNull;
 
+    private Initiative initiativeExpected;
+
+    private InitiativeDTO initiativeDTO;
+
     @BeforeEach
     public void setUp() {
         initiativeOnlyInfoGeneral = createStep1InitiativeOnlyInfoGeneral();
@@ -129,6 +133,15 @@ class InitiativeDTOsToModelMapperTest {
         initiativeRewardAndTrxRulesDTOTrxCountNull = createInitiativeRewardAndTrxRulesDTOTrxCountNull();
         initiativeTrxRuleNull = createStep4InitiativeTrxNull();
         initiativeRewardAndTrxRuleDTOTrxRuleNull = createInitiativeRewardAndTrxRulesDTOTrxRuleNull();
+
+        initiativeDTO = createStep5InitiativeDTO();
+        initiativeExpected = createStep5Initiative();
+    }
+
+    @Test
+    void toInitiativeFromInitiativeDTO_equals(){
+        Initiative initiative = initiativeDTOsToModelMapper.toInitiative(initiativeDTO);
+        assertEquals(initiativeExpected, initiative);
     }
 
     @Test
@@ -1300,27 +1313,14 @@ class InitiativeDTOsToModelMapperTest {
         return initiative;
     }
     private Initiative createStep5Initiative () {
-        Initiative initiative = new Initiative();
+        Initiative initiative = createStep4Initiative();
         return initiative;
     }
 
     private InitiativeDTO createStep5InitiativeDTO () {
-        InitiativeDTO initiativeDTO = new InitiativeDTO();
+        InitiativeDTO initiativeDTO = createStep4InitiativeDTO();
         return initiativeDTO;
     }
-
-    private Initiative createStep6Initiative () {
-        Initiative initiative = new Initiative();
-        return initiative;
-    }
-
-    private InitiativeDTO createStep6InitiativeDTO () {
-        InitiativeDTO initiativeDTO = new InitiativeDTO();
-        return initiativeDTO;
-    }
-
-
-
 
 
     private AccumulatedAmountDTO createAccumulatedAmountDTOValid(){
