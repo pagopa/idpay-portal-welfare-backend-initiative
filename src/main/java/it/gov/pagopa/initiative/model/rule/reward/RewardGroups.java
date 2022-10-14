c2fd7f177048e6a8eb0ad352a511d565cda98ba4
Stefano Terragni <stefano.terragni@everis.com>
2022-10-14 11:49:04 +0200

    DEV-Fix Missing _ for rewardRules _type on Model before Serialization to RuleEngine

--- src/main/java/it/gov/pagopa/initiative/model/rule/reward/RewardGroups.java
+++ src/main/java/it/gov/pagopa/initiative/model/rule/reward/RewardGroups.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.initiative.model.rule.reward;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class RewardGroups implements InitiativeRewardRule {
+    @JsonProperty("_type")
     private String type;
     private List<RewardGroup> rewardGroups;
 
