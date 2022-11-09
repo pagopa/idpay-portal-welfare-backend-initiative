7237dbdaec267410f7e1e936fd57e4269b337d65
Francesco Trinca <francesco.trinca@nttdata.com>
2022-11-09 20:27:17 +0100

    fix pageable

--- src/main/java/it/gov/pagopa/initiative/connector/onboarding/OnboardingRestClient.java
+++ src/main/java/it/gov/pagopa/initiative/connector/onboarding/OnboardingRestClient.java
@@ -4,6 +4,7 @@ import it.gov.pagopa.initiative.dto.ResponseOnboardingDTO;
 import java.time.LocalDateTime;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +22,10 @@ public interface OnboardingRestClient {
   @ResponseBody
   ResponseOnboardingDTO getOnboarding(
       @PathVariable("initiativeId") String initiativeId,
-      @RequestParam(required = false) Pageable pageable,
+      Pageable pageable,
       @RequestParam(required = false) String userId,
-      @RequestParam(required = false) LocalDateTime startDate,
-      @RequestParam(required = false) LocalDateTime endDate,
+      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
       @RequestParam(required = false) String status);
 
 }
