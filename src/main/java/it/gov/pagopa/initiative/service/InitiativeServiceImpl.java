1d2af55f109376cc462bc06dab39ef614bd3c62b
Marco Di Lisa <marco.di.lisa@nttdata.com>
2023-01-25 11:39:39 +0100

    [IDP-178] fix: Added update to group when changed status to IN_REVISION

--- src/main/java/it/gov/pagopa/initiative/service/InitiativeServiceImpl.java
+++ src/main/java/it/gov/pagopa/initiative/service/InitiativeServiceImpl.java
@@ -210,6 +210,7 @@ public class InitiativeServiceImpl extends InitiativeServiceRoot implements Init
             }
             initiative.setStatus(InitiativeConstants.Status.IN_REVISION);
             utilities.initiativeInRevision(this.getUserId(),initiativeId);
+            groupRestConnector.setGroupStatusToValidated(initiativeId);
             log.info("[UPDATE_TO_IN_REVISION_STATUS] - Initiative: {}. Status successfully set to IN_REVISION.", initiativeId);
         }
         else{
@@ -244,7 +245,6 @@ public class InitiativeServiceImpl extends InitiativeServiceRoot implements Init
         utilities.initiativeToCheck(this.getUserId(), initiativeId);
         log.info("[UPDATE_TO_CHECK_STATUS] - Initiative: {}. Status successfully changed", initiative.getInitiativeId());
         this.sendEmailToCurrentOrg(initiative, TEMPLATE_NAME_EMAIL_INITIATIVE_STATUS, SUBJECT_CHANGE_STATE);
-        groupRestConnector.setGroupStatusToValidated(initiativeId);
     }
 
     @Override
