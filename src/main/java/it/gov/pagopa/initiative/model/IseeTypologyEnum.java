b9fce1868b1205a6c6190898f3d2f86a5a21c388
Benedetta-fabbri <Benedetta11.Fabbri@emea.nttdata.com>
2023-04-12 18:21:52 +0200

    fix isee types

--- src/main/java/it/gov/pagopa/initiative/model/IseeTypologyEnum.java
+++ src/main/java/it/gov/pagopa/initiative/model/IseeTypologyEnum.java
@@ -1,5 +1,8 @@
 package it.gov.pagopa.initiative.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum IseeTypologyEnum {
     ORDINARIO("Ordinario"),
     MINORENNE("Minorenne"),
@@ -9,8 +12,23 @@ public enum IseeTypologyEnum {
     RESIDENZIALE("Residenziale"),
     CORRENTE("Corrente");
 
-    final String value;
+    private final String value;
     IseeTypologyEnum(String value) {
         this.value = value;
+    }
+    @Override
+    @JsonValue
+    public String toString() {
+        return value;
+    }
+
+    @JsonCreator
+    public static IseeTypologyEnum fromValue(String text) {
+        for (IseeTypologyEnum b : IseeTypologyEnum.values()) {
+            if (String.valueOf(b.value).equals(text)) {
+                return b;
+            }
+        }
+        return null;
     }
 }
