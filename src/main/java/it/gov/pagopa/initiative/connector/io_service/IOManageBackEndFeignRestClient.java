28c21fb4a28c73b25151ae65b393ed02257b185d
Giulia Tremolada <giulia.tremolada@nttdata.com>
2023-10-11 16:13:46 +0200

    [IDP-1870] fix send Logo IO

--- src/main/java/it/gov/pagopa/initiative/connector/io_service/IOManageBackEndFeignRestClient.java
+++ src/main/java/it/gov/pagopa/initiative/connector/io_service/IOManageBackEndFeignRestClient.java
@@ -32,7 +32,7 @@ public interface IOManageBackEndFeignRestClient {
   @ResponseBody
   ResponseEntity<Void> sendLogo(
           @PathVariable("serviceId") String serviceId,
-          @RequestBody @Valid LogoIODTO logo,
+          @RequestBody LogoIODTO logo,
           @RequestHeader("Ocp-Apim-Subscription-Key") String subscriptionKey);
 
   @PutMapping(
