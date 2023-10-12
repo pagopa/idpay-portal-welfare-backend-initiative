5772e52d4a06b7fc98a0191af6d74e1f67a5d6db
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-10-12 12:19:31 +0200

    [IDP-1870] modified produces and consumes put mapping

--- src/main/java/it/gov/pagopa/initiative/connector/io_service/IOManageBackEndFeignRestClient.java
+++ src/main/java/it/gov/pagopa/initiative/connector/io_service/IOManageBackEndFeignRestClient.java
@@ -28,7 +28,8 @@ public interface IOManageBackEndFeignRestClient {
 
   @PutMapping(
           value = "/manage/services/{serviceId}/logo",
-          produces = MediaType.APPLICATION_JSON_VALUE)
+          produces = MediaType.ALL_VALUE,
+          consumes = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   ResponseEntity<Void> sendLogo(
           @PathVariable("serviceId") String serviceId,
