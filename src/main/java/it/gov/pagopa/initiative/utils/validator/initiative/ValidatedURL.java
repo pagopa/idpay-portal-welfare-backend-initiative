48b43463f3ab92337ef523050d625830c6216c07
frtrinca <francesco.trinca@nttdata.com>
2024-02-01 15:49:53 +0100

    Refactor

--- src/main/java/it/gov/pagopa/initiative/utils/validator/initiative/ValidatedURL.java
+++ src/main/java/it/gov/pagopa/initiative/utils/validator/initiative/ValidatedURL.java
@@ -6,53 +6,32 @@ import it.gov.pagopa.initiative.utils.validator.URLValidation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidatedURL implements
         ConstraintValidator<URLValidation, InitiativeAdditionalDTO> {
 
-    private static final String TYPE_WEB = "web";
-    private static final String TYPE_EMAIL = "email";
-    private static final String TYPE_MOBILE = "mobile";
-    private static final String VALID_LINK = "^(https)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-    private static final String VALID_EMAIL = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-    private static final String VALID_MOBILE = "^[+]?\\d+";
+    private static final Map<String, String> typeAndRegex = Map.of(
+            "web", "^(https)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]",
+            "email", "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",
+            "mobile", "^[+]?\\d+");
 
     @Override
     public boolean isValid(InitiativeAdditionalDTO value, ConstraintValidatorContext context) {
-        List<ChannelDTO> list = value.getChannels();
-
-        boolean valid = true;
-
-        for(ChannelDTO c : list){
-            String code = c.getType().toString();
-            if (notValid(c, code)) return false;
+        for(ChannelDTO c : value.getChannels()){
+            if (!validContact(c, c.getType().toString())) return false;
         }
-
-        return valid;
+        return true;
     }
 
-    private static boolean notValid(ChannelDTO c, String code) {
-        if(TYPE_WEB.equals(code)){
-            Pattern patt = Pattern.compile(VALID_LINK);
-            Matcher matcher = patt.matcher(c.getContact());
-            if(!matcher.matches())
-                return true;
-        }
-        if(TYPE_EMAIL.equals(code)){
-            Pattern patt = Pattern.compile(VALID_EMAIL);
-            Matcher matcher = patt.matcher(c.getContact());
-            if(!matcher.matches())
-                return true;
-        }
-        if(TYPE_MOBILE.equals(code)){
-            Pattern patt = Pattern.compile(VALID_MOBILE);
-            Matcher matcher = patt.matcher(c.getContact());
-            return !matcher.matches();
-        }
-        return false;
+    private static boolean validContact(ChannelDTO c, String code) {
+        if(!typeAndRegex.containsKey(code)) return false;
+
+        Pattern patt = Pattern.compile(typeAndRegex.get(code));
+        Matcher matcher = patt.matcher(c.getContact());
+        return matcher.matches();
     }
 
 }
