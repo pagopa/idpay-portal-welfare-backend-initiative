8ac82b03d58a8d9fc6d3ec11a2e4afab01821bec
andreaV95 <157486351+Vitolo-Andrea@users.noreply.github.com>
2025-11-10 12:10:46 +0100

    fix: ab assistance api fix (#300)

--- src/main/java/it/gov/pagopa/assistance/controller/AssistanceController.java
+++ src/main/java/it/gov/pagopa/assistance/controller/AssistanceController.java
@@ -3,19 +3,19 @@ package it.gov.pagopa.assistance.controller;
 import it.gov.pagopa.assistance.dto.response.OnboardingStatusDTO;
 import it.gov.pagopa.assistance.dto.response.VouchersStatusDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("idpay/assistance")
 public interface AssistanceController {
 
-    @GetMapping(value = "/onboardings/status/{initiativeId}/{userId}", produces = "application/json")
+    @PostMapping(value = "/onboardings/status/{initiativeId}/{userId}", produces = "application/json")
     ResponseEntity<OnboardingStatusDTO> onboardingStatus(
             @PathVariable("initiativeId") String initiativeId,
             @PathVariable("userId") String userId
     );
 
-    @GetMapping(value = "/vouchers/status/{initiativeId}/{userId}", produces = "application/json")
+    @PostMapping(value = "/vouchers/status/{initiativeId}/{userId}", produces = "application/json")
     ResponseEntity<VouchersStatusDTO> vouchersStatus(
             @PathVariable("initiativeId") String initiativeId,
             @PathVariable("userId") String userId
